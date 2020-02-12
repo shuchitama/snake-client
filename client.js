35d34c3ab521264c7e2703e754ddc2146b12ddce
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-12 13:51:11 -0800

    set client initials

--- client.js
+++ client.js
@@ -12,6 +12,10 @@ const connect = function () {
   conn.on('data', (data) => {       // allows client to see message sent by server
     console.log('Server says: ', data);
   });
+  conn.on('connect', () => { // connect handler ( is a cb function) is triggered as soon as connection is established
+    console.log('Successfully connected to game server');
+    conn.write('Name: SJM');
+  });
 
   return conn;
 }
