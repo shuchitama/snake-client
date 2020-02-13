6cf2a4295b5bb743b078046acf664c4628fc9f17
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-12 16:25:22 -0800

    implement movement

--- input.js
+++ input.js
@@ -1,11 +1,11 @@
-const net = require('net'); // net is part of node, so we don't have to install it like we do for chai
-const stdin = process.stdin;
+// const net = require('net'); // net is part of node, so we don't have to install it like we do for chai
+const stdin = process.stdin;      // actively listening for keyboard input with stdin
 stdin.setEncoding('utf8');
+let connection; // allows this file to send msgs to server
 
-const setupInput = function () {
-  const stdin = process.stdin;      // actively listening for keyboard input with stdin
+const setupInput = function (conn) {
+  connection = conn;
   stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', handleUserInput);
   return stdin;
@@ -14,6 +14,18 @@ const setupInput = function () {
 const handleUserInput = (data) => {    // must hardcode exit by ^C because stdin is listening
   if (data === '\u0003') {  // '\u0003' maps to ^C
     process.exit();
+  }
+  if (data === 'w') {
+    connection.write('Move: up');
+  }
+  if (data === 's') {
+    connection.write('Move: down');
+  }
+  if (data === 'a') {
+    connection.write('Move: left');
+  }
+  if (data === 'd') {
+    connection.write('Move: right');
   }
 }
 
